0d78ac2ff517f15f3b3029152f001cc755f8b260
sugarboo <kennethbaak@gmail.com>
2023-12-12 14:21:19 +0800

    feat: add `darkMode` config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { ThemeProvider } from '@/components/providers/theme-provider';
 
 export default function RootLayout({
   children,
@@ -8,7 +9,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="color-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
