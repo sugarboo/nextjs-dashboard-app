06a50b67c46c24cbef13c02adbfedcb8788909c4
sugarboo <kennethbaak@gmail.com>
2023-12-15 16:39:02 +0800

    feat: customer page & empty view

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,13 +1,26 @@
 import { Metadata } from "next";
 
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
 export const metadata: Metadata = {
   title: 'Customers'
 }
 
-const DashboardCustomersPage = () => {
+const DashboardCustomersPage = async({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string,
+  }
+}) => {
+  const query = searchParams?.query || '';
+  // fetch customers data
+  const customers = await fetchFilteredCustomers(query);
+
   return (
     <>
-      <p>Dashboard Customer Page</p>
+      <CustomersTable customers={customers} />
     </>
   );
 }
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
 import {
-  CustomersTableType,
+  // CustomersTableType,
   FormattedCustomersTable,
 } from '@/app/lib/definitions';
 
@@ -22,45 +23,57 @@ export default async function CustomersTable({
           <div className="inline-block min-w-full align-middle">
             <div className="overflow-hidden rounded-md bg-gray-50 p-2 md:pt-0">
               <div className="md:hidden">
-                {customers?.map((customer) => (
-                  <div
-                    key={customer.id}
-                    className="mb-2 w-full rounded-md bg-white p-4"
-                  >
-                    <div className="flex items-center justify-between border-b pb-4">
-                      <div>
-                        <div className="mb-2 flex items-center">
-                          <div className="flex items-center gap-3">
-                            <Image
-                              src={customer.image_url}
-                              className="rounded-full"
-                              alt={`${customer.name}'s profile picture`}
-                              width={28}
-                              height={28}
-                            />
-                            <p>{customer.name}</p>
+                {customers && customers.length
+                  ? customers.map((customer) => (
+                    <div
+                      key={customer.id}
+                      className="mb-2 w-full rounded-md bg-white p-4"
+                    >
+                      <div className="flex items-center justify-between border-b pb-4">
+                        <div>
+                          <div className="mb-2 flex items-center">
+                            <div className="flex items-center gap-3">
+                              <Image
+                                src={customer.image_url}
+                                className="rounded-full"
+                                alt={`${customer.name}'s profile picture`}
+                                width={28}
+                                height={28}
+                              />
+                              <p>{customer.name}</p>
+                            </div>
                           </div>
+                          <p className="text-sm text-gray-500">
+                            {customer.email}
+                          </p>
                         </div>
-                        <p className="text-sm text-gray-500">
-                          {customer.email}
-                        </p>
+                      </div>
+                      <div className="flex w-full items-center justify-between border-b py-5">
+                        <div className="flex w-1/2 flex-col">
+                          <p className="text-xs">Pending</p>
+                          <p className="font-medium">{customer.total_pending}</p>
+                        </div>
+                        <div className="flex w-1/2 flex-col">
+                          <p className="text-xs">Paid</p>
+                          <p className="font-medium">{customer.total_paid}</p>
+                        </div>
+                      </div>
+                      <div className="pt-4 text-sm">
+                        <Link
+                          href={`/dashboard/invoices?query=${customer.name}`}
+                          className="text-blue-600 hover:text-blue-400"
+                        >
+                          {customer.total_invoices} invoices
+                        </Link>
                       </div>
                     </div>
-                    <div className="flex w-full items-center justify-between border-b py-5">
-                      <div className="flex w-1/2 flex-col">
-                        <p className="text-xs">Pending</p>
-                        <p className="font-medium">{customer.total_pending}</p>
-                      </div>
-                      <div className="flex w-1/2 flex-col">
-                        <p className="text-xs">Paid</p>
-                        <p className="font-medium">{customer.total_paid}</p>
-                      </div>
+                  )) 
+                  : (
+                    <div className="mb-2 w-full h-12 rounded-md bg-gray-50 p-4 text-center text-gray-400">
+                      No Data.
                     </div>
-                    <div className="pt-4 text-sm">
-                      <p>{customer.total_invoices} invoices</p>
-                    </div>
-                  </div>
-                ))}
+                  )
+                }
               </div>
               <table className="hidden min-w-full rounded-md text-gray-900 md:table">
                 <thead className="rounded-md bg-gray-50 text-left text-sm font-normal">
@@ -84,34 +97,51 @@ export default async function CustomersTable({
                 </thead>
 
                 <tbody className="divide-y divide-gray-200 text-gray-900">
-                  {customers.map((customer) => (
-                    <tr key={customer.id} className="group">
-                      <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
-                        <div className="flex items-center gap-3">
-                          <Image
-                            src={customer.image_url}
-                            className="rounded-full"
-                            alt={`${customer.name}'s profile picture`}
-                            width={28}
-                            height={28}
-                          />
-                          <p>{customer.name}</p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
-                        {customer.email}
-                      </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
-                        {customer.total_invoices}
-                      </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
-                        {customer.total_pending}
-                      </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md">
-                        {customer.total_paid}
-                      </td>
-                    </tr>
-                  ))}
+                  {customers && customers.length
+                    ? customers.map((customer) => (
+                      <tr key={customer.id} className="group">
+                        <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
+                          <div className="flex items-center gap-3">
+                            <Image
+                              src={customer.image_url}
+                              className="rounded-full"
+                              alt={`${customer.name}'s profile picture`}
+                              width={28}
+                              height={28}
+                            />
+                            <p>{customer.name}</p>
+                          </div>
+                        </td>
+                        <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                          {customer.email}
+                        </td>
+                        <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                          <Link
+                            href={`/dashboard/invoices?query=${customer.name}`}
+                            className="text-blue-600 hover:text-blue-400 hover:underline hover:underline-offset-4"
+                          >
+                            {customer.total_invoices}
+                          </Link>
+                        </td>
+                        <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                          {customer.total_pending}
+                        </td>
+                        <td className="whitespace-nowrap bg-white px-4 py-5 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md">
+                          {customer.total_paid}
+                        </td>
+                      </tr>
+                    ))
+                    :(
+                      <tr className="group">
+                        <td
+                          colSpan={5}
+                          className="w-full h-12 text-center text-gray-400"
+                        >
+                          No Data.
+                        </td>
+                      </tr>
+                    )
+                  }
                 </tbody>
               </table>
             </div>
